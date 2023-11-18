c87fb19a385e26ed59bbdb673b1af602da89957b
BilkisAkter <chumkiakter1997@gmail.com>
2023-11-18 13:04:43 -0600

    cucumber1

---
+++ src/test/java/steps/StepDefinition.java
@@ -0,0 +1,37 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition {
+
+	
+	@Given("User is on the techfios login page")
+	public void user_is_on_the_techfios_login_page() {
+	    
+	}
+
+	@When("User enters username as {string}")
+	public void user_enters_username_as(String string) {
+	    
+	}
+
+	@When("User enters password as {string}")
+	public void user_enters_password_as(String string) {
+	    
+	}
+
+	@When("User clicks on signin button")
+	public void user_clicks_on_signin_button() {
+	   
+	}
+
+	@Then("User should land on dashboard page")
+	public void user_should_land_on_dashboard_page() {
+	   
+	}
+
+
+
+}
